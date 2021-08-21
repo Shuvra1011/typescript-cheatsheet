d297eae723004834378cc3e44b673ae70c0c82d9
Shuvra1011 <devshuvra1011@gmail.com>
2021-08-21 08:23:25 +0600

    Classes and interfaces

---
+++ dist/index.js
@@ -0,0 +1,88 @@
+"use strict";
+var id = 5; // let id = 5 will infer to number. id = 'ads' will prompt an error
+var id2 = 10;
+var first_name = 'James';
+var isMount = true;
+var groceries = ['apple', 'tomato'];
+//Tuple
+var person = [56, 'sam', true];
+//Tuple Array
+var employee;
+employee = [
+    [3, 'Alex'],
+    [2, 'Crack']
+];
+//union
+var pid;
+pid = '22';
+//enum--> set of named constants
+//First two are initialised
+var NumbersV1;
+(function (NumbersV1) {
+    NumbersV1[NumbersV1["One"] = 1] = "One";
+    NumbersV1[NumbersV1["Two"] = 2] = "Two";
+    NumbersV1[NumbersV1["Three"] = 3] = "Three";
+    NumbersV1[NumbersV1["Four"] = 4] = "Four";
+})(NumbersV1 || (NumbersV1 = {}));
+var NumbersV2;
+(function (NumbersV2) {
+    NumbersV2[NumbersV2["One"] = 0] = "One";
+    NumbersV2[NumbersV2["Two"] = 1] = "Two";
+    NumbersV2[NumbersV2["Three"] = 2] = "Three";
+    NumbersV2[NumbersV2["Four"] = 3] = "Four";
+})(NumbersV2 || (NumbersV2 = {}));
+var user = {
+    id: 1,
+    name: 'Jack'
+};
+// Type Assertion--> 
+var cid = 1;
+var customerId = cid; // SAME: let customerId = cid as number
+// Functions 
+// Function parameter with implicitly typed any will report an 
+// erro. To stop this change tsconfig.noImplicitAny: false [default true]
+// let addNumV1 = (x,y)=>{
+//     return x+y;
+// }
+var addNumV2 = function (x, y) {
+    return x + y;
+};
+// console.log(addNumV2(1,'h')) [This will give an error]
+// function return type void
+function log(message) {
+    console.log(message);
+}
+var userV2 = {
+    id: 1,
+    name: 'Jack'
+};
+userV2.age = 55;
+userV2.name = "Max";
+var p1 = "Linda";
+var multiply = function (x, y) {
+    return x * y;
+};
+console.log(multiply(10, 6));
+var user3 = {
+    name: 'Alex',
+    age: 24,
+    location: 'Vancouver',
+};
+function customerInfo(_a) {
+    var name = _a.name, _b = _a.age /*default value*/, age = _b === void 0 ? 50 : _b /*default value*/, location = _a.location;
+    return name + ", is " + age + " years old and lives in " + location;
+}
+var cInfo = customerInfo(user3);
+console.log(cInfo);
+// Class
+var Fruit = /** @class */ (function () {
+    function Fruit(id, name, color, price) {
+        this.fId = id,
+            this.name = name,
+            this.color = color,
+            this.price = price;
+    }
+    return Fruit;
+}());
+var apple = new Fruit(1, 'Apple', 'Green', 20);
+var orange = new Fruit(2, 'Orange', 'Orange', 15);
